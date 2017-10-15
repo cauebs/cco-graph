10cad6a1f54dace76cb1d0981a4636d0c03e21d8
Cauê Baasch de Souza <cauebaasch@gmail.com>
2017-10-15 01:32:06 -0200

    Added curriculum planning algorithm

--- cco.py
+++ cco.py
@@ -2,6 +2,38 @@ from graph import Graph
 from curriculum import courses
 
 
-cco = Graph(courses, {(r, c)
-                      for c in courses
-                      for r in c.requirements})
+def semesters(graph, done=None, max_hours=30):
+    semester = []
+    total = 0
+    done = done or set()
+    available = {v for v in graph.vertices
+                 if not graph.predecessors(v)}
+
+    while available:
+        v = available.pop()
+        semester.append(v)
+        total += v.hours
+
+        if not available or all(total + v.hours >= max_hours
+                                for v in available):
+            done.update(semester)
+            for r in semester:
+                for c in graph.successors(r) - done:
+                    if graph.predecessors(c).issubset(done):
+                        available.add(c)
+
+            yield semester.copy()
+            semester.clear()
+            total = 0
+
+
+if __name__ == '__main__':
+    cco = Graph(courses, {(r, c)
+                          for c in courses
+                          for r in c.requirements})
+
+    for semester in semesters(cco):
+        print(sum(c.hours for c in semester))
+        for course in semester:
+            print(course.hours, course.code, course.name)
+        print()
